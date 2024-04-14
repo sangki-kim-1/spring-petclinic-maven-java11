fc622f635b8a75db1264e944ddba37dccb726130
JoisFe <rhsnqk@gmail.com>
2024-04-14 15:16:46 +0900

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,6 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
     public static void main(String[] args) {
+        System.out.println("자동재배포 테스트");
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
